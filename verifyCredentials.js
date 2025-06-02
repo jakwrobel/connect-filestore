dc04a02dcbe2e3a05d7307d60ad9c717be82840e
Jakub Wróbel <jwrobel@MacBook-Pro-Jakub.local>
2025-06-02 16:11:14 +0200

    WIP

--- verifyCredentials.js
+++ verifyCredentials.js
@@ -8,18 +8,14 @@
  * @returns boolean of whether or not the request was successful
  */
 module.exports = async function verify(credentials) {
+  this.logger.info('Executing verification logic...')
   const { apiKey } = credentials;
 
   if (!apiKey) throw new Error('API key is missing');
 
-  const client = new MagentoClient(this, credentials);
-
   try {
     // sending a request to the most simple endpoint of the target API
-    await client.makeRequest({
-      url: '/user/me',
-      method: 'GET',
-    });
+ 
 
     // if the request succeeds, we can assume the api key is valid
     this.logger.info("this is valid!")
