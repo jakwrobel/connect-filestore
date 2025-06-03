9cdd1363d129060e1d114d71ed1d8a0b1d48e9db
Jakub Wróbel <jwrobel@MacBook-Pro-Jakub.local>
2025-06-03 10:04:57 +0200

    WIP

--- verifyCredentials.js
+++ verifyCredentials.js
@@ -19,8 +19,8 @@ module.exports = async function verify(credentials) {
   if (!tenantId) throw new Error("Tenant ID is missing");
   if (!resourceServerUrl) throw new Error("Resource server URL is missing");
   const client = new FilestoreClient(this, credentials);
-  this.logger.info("The credentials: ", apiKey, tenantId, resourceServerUrl)
-    console.log("The credentials: ", apiKey, tenantId, resourceServerUrl)
+  // this.logger.info("The credentials: ", apiKey, tenantId, resourceServerUrl)
+  //   console.log("The credentials: ", apiKey, tenantId, resourceServerUrl)
 
   try {
     this.logger.info("The credentials: ", apiKey, tenantId, resourceServerUrl)
@@ -37,18 +37,18 @@ module.exports = async function verify(credentials) {
     return true;
   } catch (e) {
     this.logger.info('catch block')
-    this.logger.info('response', e.response)
-    this.logger.info('status', e.response.status)
-    if (e.response) {
-      this.logger.info('e.response block')
-      const status = e.response.status;
-      if (status === 400) {
-        this.logger.info('status 400 block')
-        return true;
-      }
-    }
-    this.logger.info('other codes error block')
+    this.logger.info('error: ', e)
+
+    // if (e.response) {
+    //   this.logger.info('e.response block')
+      // const status = e.response.status;
+      // if (status === 400) {
+      //   this.logger.info('status 400 block')
+      //   return true;
+      // }
+    // }
+    // this.logger.info('other codes error block')
     // Other cases (no response, status other than 2xx or 400)
-    throw new Error("Verification failed")
+    throw new Error("SOmething went wrong")
   }
 };
